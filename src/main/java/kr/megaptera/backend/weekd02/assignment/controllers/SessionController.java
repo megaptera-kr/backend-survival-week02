04ee4d6c791960a0cfc0e7b5bdd2cb6add7e7de6
noah <noahlog@naver.com>
2023-02-08 21:17:24 +0900

    fix: cherry-pick 병합해결

--- src/main/java/kr/megaptera/backend/weekd02/assignment/controllers/SessionController.java
+++ src/main/java/kr/megaptera/backend/weekd02/assignment/controllers/SessionController.java
@@ -1,5 +1,6 @@
 package kr.megaptera.backend.weekd02.assignment.controllers;
 
+<<<<<<< HEAD
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,4 +20,7 @@ public class SessionController {
     public String logout(@RequestAttribute String userId) {
         return "로그아웃: " + userId;
     }
+=======
+public class SessionController {
+>>>>>>> 4b8fda0 (백엔드 생존코스 2주차 과제)
 }
