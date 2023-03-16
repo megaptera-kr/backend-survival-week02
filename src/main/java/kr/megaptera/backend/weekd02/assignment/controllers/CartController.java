cccafb316b500ce2b9b418d0f5e2d8383d54e819
jack <tjdgns526@gmail.com>
2023-03-16 23:33:43 +0200

    [구성훈] 2주차 과제 - REST API

--- src/main/java/kr/megaptera/backend/weekd02/assignment/controllers/CartController.java
+++ src/main/java/kr/megaptera/backend/weekd02/assignment/controllers/CartController.java
@@ -1,4 +1,31 @@
 package kr.megaptera.backend.weekd02.assignment.controllers;
 
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/cart")
 public class CartController {
+
+    @GetMapping
+    public String list(){return "상품목록\n";}
+
+    @PostMapping()
+    @ResponseStatus(HttpStatus.CREATED)
+    public String add(
+            @RequestAttribute String userId,
+            @RequestBody(required = false) String productDTO
+    )
+    {
+        return "상품:" + productDTO+ "추가 완료\n";
+    }
+
+    @DeleteMapping("/{id}")
+    public String delete(
+            @PathVariable String id,
+            @RequestAttribute String userId){
+        return "상품 삭제" + id +"\n";
+    }
+
+
 }
--- src/main/java/kr/megaptera/backend/weekd02/assignment/controllers/ReviewController.java
+++ src/main/java/kr/megaptera/backend/weekd02/assignment/controllers/ReviewController.java
@@ -1,4 +1,38 @@
 package kr.megaptera.backend.weekd02.assignment.controllers;
 
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/products/{productId}/reviews")
 public class ReviewController {
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public String create(
+            @PathVariable String productId,
+            @RequestAttribute String userId,
+            @RequestBody(required = false) String reviewDTO
+    ) {
+        return "리뷰 작성" + reviewDTO + "\n";
+    }
+
+    @PatchMapping("/{id}")
+    public String update(
+            @PathVariable String productId,
+            @PathVariable String id,
+            @RequestAttribute String userId,
+            @RequestBody(required = false) String reviewDTO
+
+    ) {
+        return "리뷰 수정" + reviewDTO + "\n";
+    }
+
+    @DeleteMapping("/{id}")
+    public String delete(
+            @PathVariable String productId,
+            @PathVariable String id,
+            @RequestAttribute String userId
+    ) {
+        return "리뷰 삭제" + id + "\n";
+    }
 }
