ee6d40ab1d851447f7898d0df213ee3d371974b0
p-ej <show400035@naver.com>
2023-04-21 15:32:42 +0900

    feat: 로그인/회원가입, 내정보 핸들러 구현

--- src/main/java/kr/megaptera/backend/weekd02/assignment/controllers/SessionController.java
+++ src/main/java/kr/megaptera/backend/weekd02/assignment/controllers/SessionController.java
@@ -1,4 +1,25 @@
 package kr.megaptera.backend.weekd02.assignment.controllers;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
 public class SessionController {
+    @PostMapping("/login")
+    @ResponseStatus(HttpStatus.CREATED)
+    private String loginUser(@RequestBody String loginDTO) {
+        return "로그인 정보" + "'" + loginDTO + "'" + "로그인 입니다.";
+    }
+
+    @PostMapping("/logout")
+    private String logoutUser(@RequestAttribute String userId) {
+        return "'" + userId + "'" + "님 로그아웃 입니다.";
+    }
+
+    @PostMapping("/register")
+    @ResponseStatus(HttpStatus.CREATED)
+    private String createUser(@RequestBody String registerDTO) {
+        return " 회원가입 입니다. \n" + registerDTO;
+    }
 }
--- src/main/java/kr/megaptera/backend/weekd02/assignment/controllers/UserController.java
+++ src/main/java/kr/megaptera/backend/weekd02/assignment/controllers/UserController.java
@@ -1,4 +1,18 @@
 package kr.megaptera.backend.weekd02.assignment.controllers;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
+
+    @GetMapping("/me")
+    private String getUserInfo(@RequestAttribute String userId) {
+        return "내 정보 가져오기 입니다 : " + "'" + userId + "'";
+    }
+
+    @PatchMapping("/me")
+    private String updateUserInfo(@RequestAttribute String userId, @RequestBody String userUpdateDTO) {
+        return "내 정보 수정하기 입니다 : " + "'" + userId + "'" + "\n" + userUpdateDTO;
+    }
 }
