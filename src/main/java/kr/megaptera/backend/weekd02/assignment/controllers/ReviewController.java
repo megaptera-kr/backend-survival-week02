80de4d00005c49c63bc76460ec3b954a4c9ebdde
p-ej <show400035@naver.com>
2023-04-21 16:05:41 +0900

    feat: 상품 리뷰 핸들러 구현

--- src/main/java/kr/megaptera/backend/weekd02/assignment/controllers/ReviewController.java
+++ src/main/java/kr/megaptera/backend/weekd02/assignment/controllers/ReviewController.java
@@ -1,4 +1,24 @@
 package kr.megaptera.backend.weekd02.assignment.controllers;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/products/{productsId}/reviews")
 public class ReviewController {
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    private String create(@PathVariable("productsId") String productsId, @RequestBody String createReviewsDTO, @RequestAttribute String userId) {
+        return "상품 리뷰 작성 입니다.\n상품 ID : " + productsId + "\n" + "작성자 ID : " + userId + "\n" + createReviewsDTO;
+    }
+
+    @PatchMapping("/{reviewsId}")
+    private String update(@PathVariable("productsId") String productsId, @PathVariable("reviewsId") String reviewsId, @RequestBody String updateReviewsDTO, @RequestAttribute String userId) {
+        return "상품 리뷰 수정 입니다.\n상품 ID : " + productsId + "\n" + "리뷰 ID : " + reviewsId + "\n" + "작성자 ID : " + userId + "\n" + updateReviewsDTO;
+    }
+
+    @DeleteMapping("/{reviewsId}")
+    private String delete(@PathVariable("productsId") String productsId, @PathVariable("reviewsId") String reviewsId, @RequestAttribute String userId) {
+        return "상품 리뷰 삭제 입니다.\n상품 ID : " + productsId + "\n" + "리뷰 ID : " + reviewsId + "\n" + "작성자 ID : " + userId + "\n";
+    }
 }
