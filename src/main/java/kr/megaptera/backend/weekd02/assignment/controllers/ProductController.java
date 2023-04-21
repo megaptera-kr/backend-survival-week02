dbe20ce7a34b8bd48a3211f6595d0aaa5981ec14
seungjjun <il1uso@naver.com>
2023-04-21 23:20:53 +0900

    ProductController 작성

--- src/main/java/kr/megaptera/backend/weekd02/assignment/controllers/ProductController.java
+++ src/main/java/kr/megaptera/backend/weekd02/assignment/controllers/ProductController.java
@@ -1,4 +1,20 @@
 package kr.megaptera.backend.weekd02.assignment.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/products")
 public class ProductController {
+    @GetMapping
+    private String products() {
+        return "상품 목록 조회";
+    }
+
+    @GetMapping("/{productId}")
+    private String product(@PathVariable Long productId) {
+        return "상품 상세 조회";
+    }
 }
