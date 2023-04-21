b429f1a9872345b9e4ee18a9fb8995b985a735f6
p-ej <show400035@naver.com>
2023-04-21 16:18:11 +0900

    feat: 장바구니 핸들러 구현

--- src/main/java/kr/megaptera/backend/weekd02/assignment/controllers/ReviewController.java
+++ src/main/java/kr/megaptera/backend/weekd02/assignment/controllers/ReviewController.java
@@ -4,21 +4,21 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/products/{productsId}/reviews")
+@RequestMapping("/products/{productId}/reviews")
 public class ReviewController {
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    private String create(@PathVariable("productsId") String productsId, @RequestBody String createReviewsDTO, @RequestAttribute String userId) {
-        return "상품 리뷰 작성 입니다.\n상품 ID : " + productsId + "\n" + "작성자 ID : " + userId + "\n" + createReviewsDTO;
+    private String create(@PathVariable("productId") String productId, @RequestBody String createReviewsDTO, @RequestAttribute String userId) {
+        return "상품 리뷰 작성 입니다.\n상품 ID : " + productId + "\n" + "작성자 ID : " + userId + "\n" + createReviewsDTO;
     }
 
     @PatchMapping("/{reviewsId}")
-    private String update(@PathVariable("productsId") String productsId, @PathVariable("reviewsId") String reviewsId, @RequestBody String updateReviewsDTO, @RequestAttribute String userId) {
-        return "상품 리뷰 수정 입니다.\n상품 ID : " + productsId + "\n" + "리뷰 ID : " + reviewsId + "\n" + "작성자 ID : " + userId + "\n" + updateReviewsDTO;
+    private String update(@PathVariable("productId") String productId, @PathVariable("reviewId") String reviewId, @RequestBody String updateReviewsDTO, @RequestAttribute String userId) {
+        return "상품 리뷰 수정 입니다.\n상품 ID : " + productId + "\n" + "리뷰 ID : " + reviewId + "\n" + "작성자 ID : " + userId + "\n" + updateReviewsDTO;
     }
 
     @DeleteMapping("/{reviewsId}")
-    private String delete(@PathVariable("productsId") String productsId, @PathVariable("reviewsId") String reviewsId, @RequestAttribute String userId) {
-        return "상품 리뷰 삭제 입니다.\n상품 ID : " + productsId + "\n" + "리뷰 ID : " + reviewsId + "\n" + "작성자 ID : " + userId + "\n";
+    private String delete(@PathVariable("productId") String productId, @PathVariable("reviewId") String reviewId, @RequestAttribute String userId) {
+        return "상품 리뷰 삭제 입니다.\n상품 ID : " + productId + "\n" + "리뷰 ID : " + reviewId + "\n" + "작성자 ID : " + userId + "\n";
     }
 }
