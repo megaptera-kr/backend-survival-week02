15392070d7287197e875b47d014629ef150a97f8
spacekay <bethjkim0311@gmail.com>
2023-04-22 14:22:18 +0900

    [Week 2] User, Session 관련 API 설계

--- src/main/java/kr/megaptera/backend/weekd02/assignment/controllers/SessionController.java
+++ src/main/java/kr/megaptera/backend/weekd02/assignment/controllers/SessionController.java
@@ -1,4 +1,35 @@
 package kr.megaptera.backend.weekd02.assignment.controllers;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/session")
 public class SessionController {
+
+    // 로그인하기 (세션 만들기)
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public String login(
+            @RequestAttribute("userId") String userId,
+            @RequestBody String loginDTO) {
+
+        // userDTO로 들어온 userId와 password가 일치하면 로그인 성공
+
+        return userId + "번 고객님께서 로그인하셨습니다.\n";
+    }
+
+    // 로그아웃하기 (세션 파기하기)
+    @DeleteMapping
+    public String logout(
+            @RequestAttribute("userId") String userId
+    ) {
+        return userId + "번 고객님께서 성공적으로 로그아웃하셨습니다.\n";
+    }
 }
--- src/main/java/kr/megaptera/backend/weekd02/assignment/controllers/UserController.java
+++ src/main/java/kr/megaptera/backend/weekd02/assignment/controllers/UserController.java
@@ -1,4 +1,51 @@
 package kr.megaptera.backend.weekd02.assignment.controllers;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
 public class UserController {
+
+    // 회원가입 하기
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public String createUser(
+            @RequestAttribute String userId,
+            @RequestBody String userDTO
+    ) {
+        return userId + "님의 회원 가입을 축하합니다.\n";
+    }
+
+    // 자신의 정보 얻기
+    @GetMapping("/me")
+    public String getMyInfo(@RequestAttribute String userId) {
+        return userId + "님의 회원 정보입니다.\n";
+    }
+
+    // 자신의 정보 수정하기
+    @PatchMapping("/me")
+    public String patchMyInfo(
+            @RequestAttribute String userId,
+            @RequestBody String userDTO
+    ) {
+        return userId + "님의 정보를 요청 내용으로 수정합니다.\n";
+    }
+
+    // Exception handling (POST, PATCH 시 body가 없는 경우 대응)
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String badRequest() {
+        return "해당 요청 시에는 요청 내용 전송이 필요합니다.\n";
+    }
+
 }
