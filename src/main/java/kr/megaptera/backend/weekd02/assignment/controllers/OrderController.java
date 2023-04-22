7bba8203c3e6fe3d302ab5f14834b81f374ff4b7
seungjjun <il1uso@naver.com>
2023-04-22 12:51:14 +0900

    OrderController 작성

--- src/main/java/kr/megaptera/backend/weekd02/assignment/controllers/OrderController.java
+++ src/main/java/kr/megaptera/backend/weekd02/assignment/controllers/OrderController.java
@@ -1,4 +1,23 @@
 package kr.megaptera.backend.weekd02.assignment.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/orders")
 public class OrderController {
+    @GetMapping
+    private String orders() {
+        return "주문 목록";
+    }
+
+    @PostMapping
+    private String createOrder(
+        @RequestBody String orderDto
+    ) {
+        return "주문하기" + orderDto + "\n";
+    }
 }
