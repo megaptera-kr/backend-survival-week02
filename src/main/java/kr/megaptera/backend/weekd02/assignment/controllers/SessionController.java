b4eb46268c4612acb8a3ad3cac65667b18b823a8
seungjjun <il1uso@naver.com>
2023-04-22 13:19:19 +0900

    UserController 작성

--- src/main/java/kr/megaptera/backend/weekd02/assignment/controllers/SessionController.java
+++ src/main/java/kr/megaptera/backend/weekd02/assignment/controllers/SessionController.java
@@ -1,7 +1,6 @@
 package kr.megaptera.backend.weekd02.assignment.controllers;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
--- src/main/java/kr/megaptera/backend/weekd02/assignment/controllers/UserController.java
+++ src/main/java/kr/megaptera/backend/weekd02/assignment/controllers/UserController.java
@@ -1,4 +1,27 @@
 package kr.megaptera.backend.weekd02.assignment.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users/me")
 public class UserController {
+    @GetMapping
+    private String me(
+        @RequestAttribute String userId
+    ) {
+        return "내 정보 조회 : " + userId + "\n";
+    }
+
+    @PatchMapping
+    private String updateUser(
+        @RequestAttribute String userId,
+        @RequestBody String userDto
+    ) {
+        return "내 정보 수정" + userDto + "\n";
+    }
 }
