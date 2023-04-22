72d7f1774a8602d6f97e0060295d500421aa3f19
spacekay <bethjkim0311@gmail.com>
2023-04-22 15:26:11 +0900

    [Week 2] Product 관련 API 설계 (Product, Review, Cart, Order)

--- src/main/java/kr/megaptera/backend/weekd02/assignment/controllers/CartController.java
+++ src/main/java/kr/megaptera/backend/weekd02/assignment/controllers/CartController.java
@@ -1,4 +1,37 @@
 package kr.megaptera.backend.weekd02.assignment.controllers;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class CartController {
+
+    // 장바구니 조회
+    @GetMapping("cart")
+    public String getCart(
+            @RequestAttribute("userId") String userId
+    ) {
+        return userId + "님의 장바구니입니다.\n";
+    }
+
+    // 특정 상품을 장바구니에 넣기
+    @GetMapping("products/{productId}/cart")
+    public String addProductFromCart(
+            @RequestAttribute("userId") String userId,
+            @PathVariable("productId") String productId
+    ) {
+        return userId + "님이 " + productId + "번 상품을 장바구니에 넣었습니다.\n";
+    }
+
+    // 특정 상품을 장바구니에서 빼기
+    @DeleteMapping("products/{productId}/cart")
+    public String deleteProductFromCart(
+            @RequestAttribute("userId") String userId,
+            @PathVariable("productId") String productId
+    ) {
+        return userId + "님이 " + productId + "번 상품을 장바구니에서 삭제했습니다.\n";
+    }
 }
--- src/main/java/kr/megaptera/backend/weekd02/assignment/controllers/ReviewController.java
+++ src/main/java/kr/megaptera/backend/weekd02/assignment/controllers/ReviewController.java
@@ -1,4 +1,59 @@
 package kr.megaptera.backend.weekd02.assignment.controllers;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+// 상품 하나엔 리뷰를 한번씩만 작성할 수 있다고 가정함
+@RestController
+@RequestMapping("products/{productId}/review")
 public class ReviewController {
+    // 상품에 리뷰 작성
+    @PostMapping
+    public String createReview(
+            @RequestAttribute("userId") String userId,
+            @PathVariable("productId") String productId,
+            @RequestBody String reviewDTO
+    ) {
+        return userId + "님의 " + productId + " 상품에 대한 리뷰가 등록되었습니다.\n\n"
+                + reviewDTO + "\n";
+    }
+
+    // 상품에 리뷰 수정
+    @PatchMapping("/{reviewId}")
+    public String updateReview(
+            @RequestAttribute("userId") String userId,
+            @PathVariable("productId") String productId,
+            @PathVariable("reviewId") String reviewId,
+            @RequestBody String reviewDTO
+    ) {
+        return userId + "님의 " + productId + " 상품에 대한 리뷰(리뷰 번호 : " + reviewId + ")가 수정되었습니다.\n\n"
+                + reviewDTO + "\n";
+    }
+
+    // 상품에 리뷰 삭제
+    @DeleteMapping("/{reviewId}")
+    public String deleteReview(
+            @RequestAttribute("userId") String userId,
+            @PathVariable("productId") String productId,
+            @PathVariable("reviewId") String reviewId
+    ) {
+        return userId + "님의 " + productId + " 상품에 대한 리뷰(리뷰 번호 : " + reviewId + ")가 삭제되었습니다.\n";
+    }
+
+    // Exception handling (POST, PATCH 시 body가 없는 경우 대응)
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String badRequest() {
+        return "해당 요청 시에는 요청 내용 전송이 필요합니다.\n";
+    }
 }
