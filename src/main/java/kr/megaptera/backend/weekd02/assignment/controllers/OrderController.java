013aea6e3c0c846eee3bf671634ddcd7700dc6f8
seungjjun <il1uso@naver.com>
2023-04-22 19:07:25 +0900

    주문 목록 확인시 인증 추가

--- src/main/java/kr/megaptera/backend/weekd02/assignment/controllers/OrderController.java
+++ src/main/java/kr/megaptera/backend/weekd02/assignment/controllers/OrderController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/orders")
 public class OrderController {
     @GetMapping
-    private String orders() {
+    private String orders(@RequestAttribute String userId) {
         return "주문 목록";
     }
 
