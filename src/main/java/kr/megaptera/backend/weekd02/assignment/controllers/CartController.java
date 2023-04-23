e83d196171cc07acc9eb6fdb646843b70a65a396
dumbbelloper <dumbbelloper@gmail.com>
2023-04-23 18:14:45 +0900

    2주차 과제 제출

--- src/main/java/kr/megaptera/backend/weekd02/assignment/controllers/CartController.java
+++ src/main/java/kr/megaptera/backend/weekd02/assignment/controllers/CartController.java
@@ -1,4 +1,39 @@
 package kr.megaptera.backend.weekd02.assignment.controllers;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("carts")
 public class CartController {
+
+    @GetMapping
+    public String list(@RequestAttribute String userId) {
+        return "장바구니 상품 조회: " + userId + "\n";
+    }
+
+    @PostMapping("items")
+    @ResponseStatus(HttpStatus.CREATED)
+    public String create(
+            @RequestAttribute String userId,
+            @RequestBody String itemDto
+    ) {
+        return "장바구니 상품 추가" + itemDto + "\n";
+    }
+
+    @DeleteMapping("items/{id}")
+    public String delete(
+            @PathVariable String id,
+            @RequestAttribute String userId
+    ) {
+        return "장바구니 상품 삭제: " + id + "\n";
+    }
 }
--- src/main/java/kr/megaptera/backend/weekd02/assignment/controllers/UserController.java
+++ src/main/java/kr/megaptera/backend/weekd02/assignment/controllers/UserController.java
@@ -1,4 +1,35 @@
 package kr.megaptera.backend.weekd02.assignment.controllers;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("users")
 public class UserController {
+    @GetMapping
+    public String detail(@RequestAttribute String userId) {
+        return "내 정보 조회: " + userId + "\n";
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public String create(@RequestBody String userCreateDTO) {
+        return "회원가입: " + userCreateDTO + "\n";
+    }
+
+    @PatchMapping("/{id}")
+    public String update(
+            @PathVariable String id,
+            @RequestBody String userUpdateDto
+    ) {
+        return "내 정보 수정" + userUpdateDto + "\n";
+    }
 }
