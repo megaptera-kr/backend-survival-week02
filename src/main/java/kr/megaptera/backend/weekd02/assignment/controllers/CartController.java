1e98c9e88f25215ed2ea6d4f83d965a4036862a2
immsee098 <immsee098@gmail.com>
2023-04-23 23:54:36 +0900

    2주차 과제

--- src/main/java/kr/megaptera/backend/weekd02/assignment/controllers/CartController.java
+++ src/main/java/kr/megaptera/backend/weekd02/assignment/controllers/CartController.java
@@ -1,4 +1,30 @@
 package kr.megaptera.backend.weekd02.assignment.controllers;
 
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("cart")
 public class CartController {
+    @GetMapping
+    public String cartList(@RequestAttribute String userId) {
+        return "장바구니 상품 조회 : " + userId + "\n";
+    }
+
+    @PostMapping("items")
+    @ResponseStatus(HttpStatus.CREATED)
+    public String create(
+            @RequestBody String item,
+            @RequestAttribute String userId
+    ) {
+        return "장바구니 상품 추가 : " + item + "\n";
+    }
+
+    @DeleteMapping("items/{id}")
+    public String delete(
+            @PathVariable String id,
+            @RequestAttribute String userId
+    ) {
+        return "장바구니 상품 삭제 : " + id + " / 유저 id : " + userId + "\n";
+    }
 }
--- src/main/java/kr/megaptera/backend/weekd02/assignment/controllers/OrderController.java
+++ src/main/java/kr/megaptera/backend/weekd02/assignment/controllers/OrderController.java
@@ -1,4 +1,22 @@
 package kr.megaptera.backend.weekd02.assignment.controllers;
 
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("orders")
 public class OrderController {
+    @GetMapping
+    public String list(@RequestAttribute String userId) {
+        return "주문 목록 : " + userId + "\n";
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public String create(
+            @RequestAttribute String userId,
+            @RequestBody String order
+    ) {
+        return "주문하기 : " + order + " / 주문자 : " + userId + "\n";
+    }
 }
