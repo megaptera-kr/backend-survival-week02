4a8125f895fc87121ce93410291a43cf568beec0
honki12345 <iui4793@gmail.com>
2023-07-28 23:51:14 +0900

    feature: 상품 도메인의 컨트롤러와 테스트를 구현하였다.

--- src/main/java/kr/megaptera/backend/weekd02/assignment/controllers/ProductController.java
+++ src/main/java/kr/megaptera/backend/weekd02/assignment/controllers/ProductController.java
@@ -1,4 +1,25 @@
 package kr.megaptera.backend.weekd02.assignment.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/products")
 public class ProductController {
+    // 상품 목록 조회
+    @GetMapping
+    public String getProducts(@RequestAttribute String userId) {
+        return "상품 목록 조회: " + userId;
+    }
+
+    // 상품 상세 조회
+    @GetMapping("/{productId}")
+    public String getProduct(
+            @RequestAttribute String userId,
+            @PathVariable String productId) {
+        return "상품 상세 조회: " + userId + ":" + productId;
+    }
 }
