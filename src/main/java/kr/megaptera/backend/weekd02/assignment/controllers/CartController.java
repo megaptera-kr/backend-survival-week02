ab66b5a5dbea013ba0fd02f3dc26880981acb17c
soddong <aledjdygus9300@gmail.com>
2023-08-26 22:45:59 +0900

    요구사항 3. REST API 구현 - 장바구니, 주문하기

--- src/main/java/kr/megaptera/backend/weekd02/assignment/controllers/CartController.java
+++ src/main/java/kr/megaptera/backend/weekd02/assignment/controllers/CartController.java
@@ -1,4 +1,34 @@
 package kr.megaptera.backend.weekd02.assignment.controllers;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/users/{usersid}/cart")
+@RestController
 public class CartController {
+
+    @GetMapping
+    public String getList(){
+        return "장바구니 목록";
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public String create(@RequestBody String body){
+        return "장바구니 추가";
+    }
+
+    @DeleteMapping("/{cartid}")
+    public String delete(@PathVariable String cartid){
+        return "장바구니 상품 삭제";
+    }
+
 }
--- src/main/java/kr/megaptera/backend/weekd02/assignment/controllers/OrderController.java
+++ src/main/java/kr/megaptera/backend/weekd02/assignment/controllers/OrderController.java
@@ -1,4 +1,22 @@
 package kr.megaptera.backend.weekd02.assignment.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/users/{userid}/orders")
+@RestController
 public class OrderController {
+
+    @PostMapping
+    public String create(@RequestAttribute String body){
+        return "주문 하기";
+    }
+
+    @GetMapping
+    public String getList(){
+        return "주문 목록"
+    }
 }
