53fb2c47808c97b895272ffa9db7ab0269ead984
DESKTOP-LH02614\jhp52 <jhp5234@gmail.com>
2023-08-26 17:06:40 +0900

    세션 컨트롤러 구현

--- src/main/java/kr/megaptera/backend/weekd02/assignment/controllers/SessionController.java
+++ src/main/java/kr/megaptera/backend/weekd02/assignment/controllers/SessionController.java
@@ -1,4 +1,22 @@
 package kr.megaptera.backend.weekd02.assignment.controllers;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/session")
 public class SessionController {
+    @PostMapping
+    public String create(@RequestBody String sessionDTO) {
+        return "로그인\n";
+    }
+
+    @DeleteMapping
+    public String delete(@RequestAttribute String userId) {
+        return "로그아웃\n";
+    }
 }
