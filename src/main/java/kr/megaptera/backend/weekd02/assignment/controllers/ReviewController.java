4b9edfa30fa5f9ea44528643c0a98deae4345df8
DongJae <tlsehdwo7@naver.com>
2023-09-24 15:29:45 +0900

    add: week2 backend rest api design assignment

--- src/main/java/kr/megaptera/backend/weekd02/assignment/controllers/ReviewController.java
+++ src/main/java/kr/megaptera/backend/weekd02/assignment/controllers/ReviewController.java
@@ -1,4 +1,44 @@
 package kr.megaptera.backend.weekd02.assignment.controllers;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("products/{id}/comment")
 public class ReviewController {
+    @GetMapping
+    public String list(@PathVariable String id) {
+        return "리뷰 목록: " + id + "\n";
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public String create(
+            @PathVariable String id,
+            @RequestBody String reviewDto,
+            @RequestAttribute String userId
+    ) {
+        return "상품에 리뷰 작성: " + reviewDto + "\n";
+    }
+
+    @PatchMapping("{commentId}")
+    public String update(
+            @PathVariable String id,
+            @PathVariable String commentId,
+            @RequestBody String reviewDto,
+            @RequestAttribute String userId
+    ) {
+
+        return "상품에 리뷰 수정: " + reviewDto + "\n";
+    }
+
+    @DeleteMapping("{commentId}")
+    public String delete(
+            @PathVariable String id,
+            @PathVariable String commentId,
+            @RequestAttribute String userId
+    ) {
+
+        return "상품에 리뷰 삭제: " + id + "\n";
+    }
 }
--- src/main/java/kr/megaptera/backend/weekd02/assignment/controllers/UserController.java
+++ src/main/java/kr/megaptera/backend/weekd02/assignment/controllers/UserController.java
@@ -1,4 +1,27 @@
 package kr.megaptera.backend.weekd02.assignment.controllers;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("users")
 public class UserController {
+    @GetMapping("me")
+    public String detail(@RequestAttribute String userId) {
+        return "내 정보 조회: " + userId + "\n";
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public String create(@RequestBody String userCreateDTO) {
+        return "회원가입: " + userCreateDTO + "\n";
+    }
+
+    @PatchMapping("me")
+    public String update(
+            @RequestBody String userUpdateDto,
+            @RequestAttribute String userId
+    ) {
+        return "내 정보 수정" + userUpdateDto + "\n";
+    }
 }
