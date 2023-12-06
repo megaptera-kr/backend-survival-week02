0ad5168a46d78201659e93630b819c5b2c9e87d8
parkmyungkwan <pmk3762@gmail.com>
2023-12-06 23:43:37 +0900

    commit

--- src/main/java/kr/megaptera/backend/weekd02/assignment/controllers/CartController.java
+++ src/main/java/kr/megaptera/backend/weekd02/assignment/controllers/CartController.java
@@ -2,6 +2,7 @@ package kr.megaptera.backend.weekd02.assignment.controllers;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestAttribute;
@@ -13,10 +14,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("cart")
 public class CartController {
+	@GetMapping
+	public String list(@RequestAttribute String userId) {
+		return "장바구니 상품 조회: " + userId + "\n";
+	}
 	@PostMapping("/item")
 	@ResponseStatus(HttpStatus.CREATED)
 	public String create(@RequestAttribute String userId, @RequestBody String itemDto) {
-		return "장바구니 상품 추가" + itemDto + "\n";
+		return "장바구니 상품 추가:" + itemDto + "\n";
 	}
 
 	@DeleteMapping("/item/{id}")
