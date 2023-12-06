4696dac4a8921491929487c9546486b0a24272a0
parkmyungkwan <pmk3762@gmail.com>
2023-12-06 23:30:23 +0900

    2주차 과제 commit

--- src/main/java/kr/megaptera/backend/weekd02/assignment/controllers/ReviewController.java
+++ src/main/java/kr/megaptera/backend/weekd02/assignment/controllers/ReviewController.java
@@ -1,4 +1,34 @@
 package kr.megaptera.backend.weekd02.assignment.controllers;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/product/{productId}/review")
 public class ReviewController {
+	@PostMapping()
+	@ResponseStatus(HttpStatus.CREATED)
+	public String create(@PathVariable String productId, @RequestBody String reviewDto, @RequestAttribute String userId) {
+		return "상품에 리뷰 작성: " + reviewDto + "\n";
+	}
+
+	@PatchMapping("/{id}")
+	public String update(@PathVariable String productId, @PathVariable String id, @RequestBody String reviewDto, @RequestAttribute String userId) {
+		return "상품에 리뷰 수정: " + id + "\n";
+	}
+
+	@DeleteMapping("/{id}")
+	public String delete(@PathVariable String productId, @PathVariable String id, @RequestAttribute String userId) {
+		return "상품에 리뷰 삭제: " + id + "\n";
+	}
+
 }
--- src/main/java/kr/megaptera/backend/weekd02/assignment/controllers/UserController.java
+++ src/main/java/kr/megaptera/backend/weekd02/assignment/controllers/UserController.java
@@ -1,4 +1,32 @@
 package kr.megaptera.backend.weekd02.assignment.controllers;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
 public class UserController {
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public String create(@RequestBody String userCreateDTO) {
+		return "회원가입: " + userCreateDTO + "\n";
+	}
+
+	@GetMapping("/me")
+	public String myInfo(@PathVariable String userId) {
+		return "내정보 조회: " + userId + "\n";
+	}
+
+	@PatchMapping("/me")
+	public String update(@RequestBody String userUpdateDto, @RequestAttribute String userId) {
+		return "내정보 수정: " + userUpdateDto + "\n";
+	}
 }
