bf812074de1394ff1c8554ea34a2790b03f14940
PracLee <mrgoga@naver.com>
2024-02-02 21:09:08 +0900

    API 문서 보기용

--- src/main/java/kr/megaptera/backend/weekd02/assignment/controllers/CartController.java
+++ src/main/java/kr/megaptera/backend/weekd02/assignment/controllers/CartController.java
@@ -1,4 +1,9 @@
 package kr.megaptera.backend.weekd02.assignment.controllers;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class CartController {
+    
 }
