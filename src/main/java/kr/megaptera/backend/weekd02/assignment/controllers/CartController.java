74aa6ba7efec66282ff5cba93c127cc63b5c0a81
Jeongpope <nullptr-@kakao.com>
2024-02-04 18:49:51 +0900

    rest api

--- src/main/java/kr/megaptera/backend/weekd02/assignment/controllers/CartController.java
+++ src/main/java/kr/megaptera/backend/weekd02/assignment/controllers/CartController.java
@@ -1,4 +1,23 @@
 package kr.megaptera.backend.weekd02.assignment.controllers;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/carts")
 public class CartController {
+
+    @GetMapping
+    public String getCarts() {
+        return "Carts are returned";
+    }
+
+    @PostMapping("/{productId}")
+    public String addProductToCart(@PathVariable String productId) {
+        return "Product " + productId + " is added to cart";
+    }
+
+    @DeleteMapping("/{productId}")
+    public String deleteProductFromCart(@PathVariable String productId) {
+        return "Product " + productId + " is deleted from cart";
+    }
 }
--- src/main/java/kr/megaptera/backend/weekd02/assignment/controllers/ReviewController.java
+++ src/main/java/kr/megaptera/backend/weekd02/assignment/controllers/ReviewController.java
@@ -1,4 +1,23 @@
 package kr.megaptera.backend.weekd02.assignment.controllers;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/reviews")
 public class ReviewController {
+
+    @GetMapping("/{productId}")
+    public String getReviews(@PathVariable String productId) {
+        return "Reviews of product " + productId + " are returned";
+    }
+
+    @PatchMapping("/{productId}/{reviewId}")
+    public String updateReview(@PathVariable String productId, @PathVariable String reviewId) {
+        return "Review " + reviewId + " of product " + productId + " is updated";
+    }
+
+    @DeleteMapping("/{productId}/{reviewId}")
+    public String deleteReview(@PathVariable String productId, @PathVariable String reviewId) {
+        return "Review " + reviewId + " of product " + productId + " is deleted";
+    }
 }
